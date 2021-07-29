0b843edceb0b686c2eadfe6544c77564a0e4468a
hankyul <consistant1y@ajou.ac.kr>
2021-07-29 12:33:30 +0900

    style: refactoring transformer.py code
    I replace transformer code for more clean view.
    - model
    - test
    - sample input

--- src/transformer.py
+++ src/transformer.py
@@ -6,137 +6,81 @@ import numpy as np
 import torch
 from einops import rearrange
 from torch import nn
-from torch.nn import functional as F
+import torch.nn.functional as F
 
 
-class Embed(nn.Module):
-    def __init__(self, d_model, vocab_size):
-        super(Embed, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, d_model)
+class Embedding(nn.Module):
+    def __init__(self, vocab_size, d_model):
+        super(Embedding, self).__init__()
+        self.embed = nn.Embedding(vocab_size, d_model)
         self.d_model = d_model
 
     def forward(self, x):
-        return self.embedding(x) * math.sqrt(self.d_model)
+        return self.embed(x) * math.sqrt(self.d_model)
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, max_len=5000, dropout=0.1):
+    def __init__(self, d_model, max_len=50000, dropout=0.1):
         super(PositionalEncoding, self).__init__()
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(max_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000) / d_model))
-        pe[:, ::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        self.register_buffer('pe', pe.unsqueeze(0))
         self.dropout = nn.Dropout(p=dropout)
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, 1).unsqueeze(1)
+        mul_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe[:, ::2] = torch.sin(position * mul_term)
+        pe[:, 1::2] = torch.cos(position * mul_term)
+        self.register_buffer('pe', pe.unsqueeze(0))
 
     def forward(self, x):
-        return x + self.dropout(self.pe[:, :x.size(1)])
+        return self.dropout(x + self.pe[:, :x.size(1)].clone().detach())
 
 
-class Decoder(nn.Module):
-    def __init__(self, layer, norm, N):
-        super(Decoder, self).__init__()
-        self.layers = clone(layer, N)
-        self.norm = norm
-
-    def forward(self, src_feature, src_mask, tgt_embed, tgt_mask):
-        for layer in self.layers:
-            tgt_embed = layer(src_feature, src_mask, tgt_embed, tgt_mask)
-        return self.norm(tgt_embed)
-
-
-def clone(layer, N):
-    return nn.ModuleList([copy.deepcopy(layer) for _ in range(N)])
-
-
-class Encoder(nn.Module):
-    def __init__(self, layer, norm, N):
-        super(Encoder, self).__init__()
-        self.layers = clone(layer, N)
-        self.norm = norm
-
-    def forward(self, src_embed, src_mask):
-        for layer in self.layers:
-            src_embed = layer(src_embed, src_mask)
-        return self.norm(src_embed)
-
-
-class EncoderLayer(nn.Module):
-    def __init__(self, attn, ff, su):
-        super(EncoderLayer, self).__init__()
-        self.attn = attn
-        self.ff = ff
-        self.su = clone(su, 2)
-
-    def forward(self, src, src_mask):
-        src = self.su[0](src, lambda x: self.attn(src, src, src, src_mask))
-        return self.su[1](src, self.ff)
-
-
-class DecoderLayer(nn.Module):
-    def __init__(self, attn, attn2, ff, su):
-        super(DecoderLayer, self).__init__()
-        self.attn = attn
-        self.attn2 = attn2
-        self.ff = ff
-        self.su = clone(su, 3)
-
-    def forward(self, src, src_mask, tgt, tgt_mask):
-        tgt = self.su[0](tgt, lambda x: self.attn(x, x, x, tgt_mask))
-        tgt = self.su[1](tgt, lambda x: self.attn2(x, src, src, src_mask))
-        return self.su[2](tgt, self.ff)
+def clone(layer, n):
+    return nn.ModuleList([copy.deepcopy(layer) for _ in range(n)])
 
 
 class MultiheadAttention(nn.Module):
-    def __init__(self, d_model, h, dropout):
+    def __init__(self, d_model, h, dropout=0.1):
         super(MultiheadAttention, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
         assert d_model % h == 0
         self.h = h
-        self.d_model = d_model
-        self.d_k = d_model // h
-
+        self.d_k = d_model//h
         self.w = clone(nn.Linear(d_model, d_model), 3)
+        self.dropout = nn.Dropout(p=dropout)
         self.out = nn.Linear(d_model, d_model)
 
-        self.attn = None
-
     def forward(self, q, k, v, mask):
-        b, n, d_model = list(q.shape)
-        q, k, v = [rearrange(f(x), 'b n (h d) -> b h n d', h=self.h, d=self.d_k) for f, x in zip(self.w, [q, k, v])]
-        score = q @ k.transpose(-1, -2)
-        if mask is not None:
-            score = score.masked_fill(mask, -1e9)
-        self.attn = attn = F.softmax(score, dim=-1)
+        q, k, v = [rearrange(f(x), 'b s (h k) -> b h s k', h=self.h) for f, x in zip(self.w, [q, k, v])]
+        scores = q @ k.transpose(-1, -2) / math.sqrt(self.d_k)
+        scores = torch.masked_fill(scores, mask, -1e9)
+        attn = F.softmax(scores, dim=-1)
         attn = self.dropout(attn)
-        x = rearrange(attn @ v, 'b h n d -> b n (h d)', h=self.h, d=self.d_k)
-        x = self.out(x)
-        return x
+        w_v = attn @ v
+        c_v = rearrange(w_v, 'b h s k -> b s (h k)')
+        return self.out(c_v)
 
 
-class PositionwiseFeedForward(nn.Module):
+class PositionwiseFeedforward(nn.Module):
     def __init__(self, d_model, d_ff, dropout=0.1):
-        super(PositionwiseFeedForward, self).__init__()
+        super(PositionwiseFeedforward, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
         self.w_1 = nn.Linear(d_model, d_ff)
         self.w_2 = nn.Linear(d_ff, d_model)
-        self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x):
-        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+        return self.w_2(self.dropout(self.w_1(x)))
 
 
 class LayerNorm(nn.Module):
     def __init__(self, d_model, eps=1e-6):
         super(LayerNorm, self).__init__()
+        self.eps = eps
         self.a_2 = nn.Parameter(torch.ones(d_model))
         self.b_2 = nn.Parameter(torch.zeros(d_model))
-        self.eps = eps
 
     def forward(self, x):
         mean = x.mean(dim=-1, keepdim=True)
         std = x.std(dim=-1, keepdim=True)
-        return self.a_2*(x-mean)/(std + self.eps) + self.b_2
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
 
 
 class SublayerConnection(nn.Module):
@@ -149,57 +93,135 @@ class SublayerConnection(nn.Module):
         return x + self.dropout(layer(self.norm(x)))
 
 
+class EncoderLayer(nn.Module):
+    def __init__(self, attn, ff, su):
+        super(EncoderLayer, self).__init__()
+        self.attn = attn
+        self.ff = ff
+        self.su = clone(su, 2)
+
+    def forward(self, src, src_mask):
+        src = self.su[0](src, lambda x: self.attn(x, x, x, src_mask))
+        return self.su[0](src, self.ff)
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self, attn, attn2, ff, su):
+        super(DecoderLayer, self).__init__()
+        self.attn = attn
+        self.attn2 = attn2
+        self.ff = ff
+        self.su = clone(su, 3)
+
+    def forward(self, src, tgt, src_mask, tgt_mask):
+        tgt = self.su[0](tgt, lambda x: self.attn(x, x, x, tgt_mask))
+        tgt = self.su[1](tgt, lambda x: self.attn2(x, src, src, src_mask))
+        return self.su[1](tgt, self.ff)
+
+
+class Encoder(nn.Module):
+    def __init__(self, layer, norm, N):
+        super(Encoder, self).__init__()
+        self.layers = clone(layer, N)
+        self.norm = norm
+
+    def forward(self, src, src_mask):
+        for layer in self.layers:
+            src = layer(src, src_mask)
+        return self.norm(src)
+
+
+class Decoder(nn.Module):
+    def __init__(self, layer, norm, N):
+        super(Decoder, self).__init__()
+        self.layers = clone(layer, N)
+        self.norm = norm
+
+    def forward(self, src, tgt, src_mask, tgt_mask):
+        for layer in self.layers:
+            tgt = layer(src, tgt, src_mask, tgt_mask)
+        return self.norm(tgt)
+
+
 class Transformer(nn.Module):
-    def __init__(self, src_emb: nn.Sequential, tgt_emb: nn.Sequential, encoder: Encoder, decoder: Decoder,
-                 generator: nn.Linear):
+    def __init__(self, src_embed, tgt_embed, encoder, decoder, generator):
         super(Transformer, self).__init__()
-        self.generator = generator
-        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
         self.encoder = encoder
-        self.tgt_emb = tgt_emb
-        self.src_emb = src_emb
+        self.decoder = decoder
+        self.generator = generator
 
-    def forward(self, src, src_mask, tgt, tgt_mask):
-        src_feature = self.encode(src, src_mask)
-        tgt_feature = self.decode(src_feature, src_mask, tgt, tgt_mask)
-        out = self.generator(tgt_feature)
-        return out
+    def forward(self, src, tgt, src_mask, tgt_mask):
+        src_rep = self.encode(src, src_mask)
+        tgt_rep = self.decode(src_rep, tgt, src_mask, tgt_mask)
+        return self.generator(tgt_rep)
 
-    def decode(self, src_feature, src_mask, tgt, tgt_mask):
-        tgt_embed = self.tgt_emb(tgt)
-        tgt_feature = self.decoder(src_feature, src_mask, tgt_embed, tgt_mask)
-        return tgt_feature
+    def decode(self, src_rep, tgt, src_mask, tgt_mask):
+        return self.decoder(src_rep, self.tgt_embed(tgt), src_mask, tgt_mask)
 
     def encode(self, src, src_mask):
-        src_embed = self.src_emb(src)
-        src_feature = self.encoder(src_embed, src_mask)
-        return src_feature
+        return self.encoder(self.src_embed(src), src_mask)
 
-def transformer(src_vocab_size, tgt_vocab_size, d_model=512, h=2, d_ff=20, N=6):
-    x = torch.arange(100).view(10, 10)
-    mask = torch.zeros(10, 1, 10, 10)
-    tgt = torch.arange(1, 101).view(10, 10)
-    tgt_mask = (torch.zeros(10, 1, 10, 10) == 1) | (torch.from_numpy(np.triu(np.ones((10, 10)), 1)) == 1)
+    def greedy_decode(self, src, src_mask, device, max_len=20, BOS=1, EOS=None):
+        memory = self.encode(src, src_mask)
+        ys = torch.ones(1, 1).fill_(BOS).type_as(src.data)
+        for i in range(max_len - 1):
+            out = self.decode(memory, ys, src_mask, get_seq_mask(ys).to(device))
+            prob = self.generator(out[:, -1])
+            _, next_word = torch.max(prob, dim=1)
+            next_word = next_word.data[0]
+            ys = torch.cat([ys, torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
+            if next_word == EOS:
+                break
+        return ys
 
+
+def get_model(src_vocab_size, tgt_vocab_size, d_model=512, N=6, d_ff=2048, h=8, dropout=0.1):
     c = copy.deepcopy
 
-    src_emb = nn.Sequential(Embed(d_model, src_vocab_size), PositionalEncoding(d_model))
-    tgt_emb = nn.Sequential(Embed(d_model, tgt_vocab_size), PositionalEncoding(d_model))
+    src_embed = nn.Sequential(Embedding(src_vocab_size, d_model), PositionalEncoding(d_model, dropout=dropout))
+    tgt_embed = nn.Sequential(Embedding(tgt_vocab_size, d_model), PositionalEncoding(d_model, dropout=dropout))
 
-    attn = MultiheadAttention(d_model, h, dropout=0.1)
-    ff = PositionwiseFeedForward(d_model, d_ff, dropout=0.1)
-    connection = SublayerConnection(d_model, dropout=0.1)
+    attn = MultiheadAttention(d_model, h, dropout=dropout)
+    ff = PositionwiseFeedforward(d_model, d_ff, dropout=dropout)
     norm = LayerNorm(d_model)
+    su = SublayerConnection(d_model, dropout=dropout)
 
-    encoder_layer = EncoderLayer(c(attn), c(ff), c(connection))
-    encoder = Encoder(encoder_layer, c(norm), N)
-
-    decoder_layer = DecoderLayer(c(attn), c(attn), c(ff), c(connection))
-    decoder = Decoder(decoder_layer, c(norm), N)
-
+    encoder = Encoder(EncoderLayer(c(attn), c(ff), c(su)), c(norm), N)
+    decoder = Decoder(DecoderLayer(c(attn), c(attn), c(ff), c(su)), c(norm), N)
     generator = nn.Linear(d_model, tgt_vocab_size)
-    model = Transformer(src_emb, tgt_emb, encoder, decoder, generator)
-    out = model(x, mask, tgt, tgt_mask)
-    assert list(out.shape) == [10, 10, 101]
+
+    model = Transformer(src_embed, tgt_embed, encoder, decoder, generator)
+
+    for name, param in model.named_parameters():
+        if param.dim() > 1:
+            nn.init.xavier_uniform_(param.data)
 
     return model
+
+
+def get_sample_input(pad_idx=0, batch_size=10):
+    # src = torch.arange(2, 102, 1).view(10, 10)
+    # tgt_output = torch.arange(3, 103, 1).view(10, 10)
+    # tgt_input = torch.cat([torch.ones(10).unsqueeze(1), tgt_output[:, :-1].clone().detach()], dim=1).long()
+    src = torch.cat([torch.ones(batch_size).unsqueeze(1), torch.randint(2, 12, size=(batch_size, 9))], dim=1).long()
+    tgt_output = src.clone().detach()
+    tgt_input = torch.cat([torch.ones(batch_size).unsqueeze(1), tgt_output[:, :-1].clone().detach()], dim=1).long()
+    src_mask, tgt_mask = get_mask(src, tgt_input, pad_idx)
+
+    return src, tgt_input, tgt_output, src_mask, tgt_mask
+
+
+def get_mask(src, tgt, pad_idx):
+    src_mask = get_pad_mask(src, pad_idx)
+    tgt_mask = get_pad_mask(tgt, pad_idx) | get_seq_mask(tgt)
+    return src_mask, tgt_mask
+
+
+def get_seq_mask(tgt):
+    return torch.from_numpy(np.triu(np.ones(tgt.size(1)), k=1)) == 1
+
+
+def get_pad_mask(src, pad_idx):
+    return (src == pad_idx).unsqueeze(1).unsqueeze(1)
--- test/test_transformer.py
+++ test/test_transformer.py
@@ -1,166 +1,85 @@
-import copy
-
-import numpy as np
-
-import pytest
-import torch
-from torch import nn
-
-from src.transformer import Transformer, Encoder, Decoder, PositionalEncoding, Embed, MultiheadAttention, \
-    PositionwiseFeedForward, SublayerConnection, EncoderLayer, LayerNorm, DecoderLayer
+from src.transformer import get_model, get_sample_input, Embedding, PositionalEncoding, \
+    MultiheadAttention, PositionwiseFeedforward, LayerNorm, SublayerConnection
 
 
-def test_transformer_constructor():
-    d_model = 10
-    h = 2
-    d_ff = 20
-    N = 6
-    src_vocab_size = 10
-    tgt_vocab_size = 10
-    c = copy.deepcopy
-
-    src_emb = nn.Sequential(Embed(d_model, src_vocab_size), PositionalEncoding(d_model))
-    tgt_emb = nn.Sequential(Embed(d_model, tgt_vocab_size), PositionalEncoding(d_model))
-
-    attn = MultiheadAttention(d_model, h, dropout=0.1)
-    ff = PositionwiseFeedForward(d_model, d_ff, dropout=0.1)
-    connection = SublayerConnection(d_model, dropout=0.1)
-    norm = LayerNorm(d_model)
-
-    encoder_layer = EncoderLayer(c(attn), c(ff), c(connection))
-    encoder = Encoder(encoder_layer, c(norm), N)
-
-    decoder_layer = DecoderLayer(c(attn), c(attn), c(ff), c(connection))
-    decoder = Decoder(decoder_layer, c(norm), N)
-
-    generator = nn.Linear(d_model, tgt_vocab_size)
-    model = Transformer(src_emb, tgt_emb, encoder, decoder, generator)
-    assert model is not None
+def test_transformer():
+    src, tgt_input, tgt_output, src_mask, tgt_mask = get_sample_input()
+    model = get_model(102, 103, 512, 6, 2048, 8, 0.1)
+    out = model(src, tgt_input, src_mask, tgt_mask)
+    assert list(out.shape) == [10, 10, 103]
 
 
-def test_transformer_forward():
-    x = torch.arange(100).view(10, 10)
-    mask = torch.zeros(10, 1, 10, 10)
-    tgt = torch.arange(1, 101).view(10, 10)
-    tgt_mask = (torch.zeros(10, 1, 10, 10) == 1) | (torch.from_numpy(np.triu(np.ones((10, 10)), 1)) == 1)
-
-    d_model = 10
-    h = 2
-    d_ff = 20
-    N = 6
-    src_vocab_size = 101
-    tgt_vocab_size = 101
-    c = copy.deepcopy
-
-    src_emb = nn.Sequential(Embed(d_model, src_vocab_size), PositionalEncoding(d_model))
-    tgt_emb = nn.Sequential(Embed(d_model, tgt_vocab_size), PositionalEncoding(d_model))
-
-    attn = MultiheadAttention(d_model, h, dropout=0.1)
-    ff = PositionwiseFeedForward(d_model, d_ff, dropout=0.1)
-    connection = SublayerConnection(d_model, dropout=0.1)
-    norm = LayerNorm(d_model)
-
-    encoder_layer = EncoderLayer(c(attn), c(ff), c(connection))
-    encoder = Encoder(encoder_layer, c(norm), N)
-
-    decoder_layer = DecoderLayer(c(attn), c(attn), c(ff), c(connection))
-    decoder = Decoder(decoder_layer, c(norm), N)
-
-    generator = nn.Linear(d_model, tgt_vocab_size)
-    model = Transformer(src_emb, tgt_emb, encoder, decoder, generator)
-    out = model(x, mask, tgt, tgt_mask)
-    assert list(out.shape) == [10, 10, 101]
+def test_get_sample_data():
+    src, tgt_input, tgt_output, src_mask, tgt_mask = get_sample_input()
+    assert list(src_mask.shape) == [10, 1, 1, 10]
+    assert list(tgt_mask.shape) == [10, 1, 10, 10]
+    assert list(src.shape) == [10, 10]
+    assert list(tgt_input.shape) == [10, 10]
 
 
-def test_embed_constructor():
-    embed = Embed(10, 10)
-    assert embed.d_model == 10
+def test_embedding():
+    src, tgt_input, tgt_output, src_mask, tgt_mask = get_sample_input()
+    embed = Embedding(102, 50)
+    src = embed(src)
+    assert list(src.shape) == [10, 10, 50]
 
 
-def test_embed_forward():
-    embed = Embed(10, 10)
-    x = torch.arange(10).unsqueeze(0)
-    x_embed = embed(x)
-    assert list(x_embed.shape) == [1, 10, 10]
+def test_positional_encoding():
+    src, tgt_input, tgt_output, src_mask, tgt_mask = get_sample_input()
+    embed = Embedding(102, 50)
+    encoding = PositionalEncoding(50)
+    src = embed(src)
+    src = encoding(src)
+    assert list(src.shape) == [10, 10, 50]
 
 
-def test_positional_encoding_constructor():
-    encoding = PositionalEncoding(d_model=10, max_len=5000, dropout=0.1)
-    assert list(encoding.pe.shape) == [1, 5000, 10]
+def test_multihead_attention():
+    src, tgt_input, tgt_output, src_mask, tgt_mask = get_sample_input()
+    embed = Embedding(102, 50)
+    encoding = PositionalEncoding(50)
+    src = embed(src)
+    src = encoding(src)
+    attn = MultiheadAttention(50, 5)
+    src = attn(src, src, src, src_mask)
+    assert list(src.shape) == [10, 10, 50]
 
 
-def test_positional_encoding_forward():
-    x = torch.rand(10, 10, 10)
-    encoding = PositionalEncoding(d_model=10, max_len=5000, dropout=0.1)
-    x = encoding(x)
-    assert list(x.shape) == [10, 10, 10]
+def test_feedforward():
+    src, tgt_input, tgt_output, src_mask, tgt_mask = get_sample_input()
+    embed = Embedding(102, 50)
+    encoding = PositionalEncoding(50)
+    src = embed(src)
+    src = encoding(src)
+    attn = MultiheadAttention(50, 5)
+    src = attn(src, src, src, src_mask)
+    ff = PositionwiseFeedforward(50, 100)
+    src = ff(src)
+    assert list(src.shape) == [10, 10, 50]
 
 
-def test_multihead_attention_forward():
-    x = torch.rand(10, 10, 10)
-    mask = torch.zeros(10, 1, 10, 10)
-    attn = MultiheadAttention(d_model=10, h=2, dropout=0.1)
-    x = attn(x, x, x, mask)
-    assert list(x.shape) == [10, 10, 10]
+def test_norm_layer():
+    src, tgt_input, tgt_output, src_mask, tgt_mask = get_sample_input()
+    embed = Embedding(102, 50)
+    encoding = PositionalEncoding(50)
+    src = embed(src)
+    src = encoding(src)
+    attn = MultiheadAttention(50, 5)
+    src = attn(src, src, src, src_mask)
+    ff = PositionwiseFeedforward(50, 100)
+    src = ff(src)
+    norm = LayerNorm(50)
+    src = norm(src)
+    assert list(src.shape) == [10, 10, 50]
 
 
-def test_positionwisefeedforward_forward():
-    x = torch.rand(10, 10, 10)
-    ff = PositionwiseFeedForward(d_model=10, d_ff=20, dropout=0.1)
-    x = ff(x)
-    assert list(x.shape) == [10, 10, 10]
-
-
-def test_sublayerconnection_forward():
-    x = torch.rand(10, 10, 10)
-    connection = SublayerConnection(d_model=10, dropout=0.1)
-    ff = PositionwiseFeedForward(d_model=10, d_ff=20, dropout=0.1)
-    x = connection(x, ff)
-    assert list(x.shape) == [10, 10, 10]
-
-
-def test_encoder_layer_forward():
-    x = torch.rand(10, 10, 10)
-    mask = torch.zeros(10, 1, 10, 10)
-    attn = MultiheadAttention(d_model=10, h=2, dropout=0.1)
-    ff = PositionwiseFeedForward(d_model=10, d_ff=20, dropout=0.1)
-    connection = SublayerConnection(d_model=10, dropout=0.1)
-    encoder_layer = EncoderLayer(attn, ff, connection)
-    x = encoder_layer(x, mask)
-    assert list(x.shape) == [10, 10, 10]
-
-
-def test_encoder_forward():
-    c = copy.deepcopy
-    x = torch.rand(10, 10, 10)
-    mask = torch.zeros(10, 1, 10, 10)
-    attn = MultiheadAttention(d_model=10, h=2, dropout=0.1)
-    ff = PositionwiseFeedForward(d_model=10, d_ff=20, dropout=0.1)
-    connection = SublayerConnection(d_model=10, dropout=0.1)
-    encoder_layer = EncoderLayer(c(attn), c(ff), c(connection))
-    norm = LayerNorm(d_model=10)
-    encoder = Encoder(encoder_layer, norm, 6)
-    x = encoder(x, mask)
-    assert list(x.shape) == [10, 10, 10]
-
-
-def test_decoder_forward():
-    c = copy.deepcopy
-    x = torch.rand(10, 10, 10)
-    mask = torch.zeros(10, 1, 10, 10)
-    tgt = torch.rand(10, 10, 10)
-    tgt_mask = (torch.zeros(10, 1, 10, 10) == 1) | (torch.from_numpy(np.triu(np.ones((10, 10)), 1)) == 1)
-
-    attn = MultiheadAttention(d_model=10, h=2, dropout=0.1)
-    ff = PositionwiseFeedForward(d_model=10, d_ff=20, dropout=0.1)
-    connection = SublayerConnection(d_model=10, dropout=0.1)
-    norm = LayerNorm(d_model=10)
-
-    encoder_layer = EncoderLayer(c(attn), c(ff), c(connection))
-    encoder = Encoder(encoder_layer, c(norm), 6)
-    src_feature = encoder(x, mask)
-
-    decoder_layer = DecoderLayer(c(attn), c(attn), c(ff), c(connection))
-    decoder = Decoder(decoder_layer, c(norm), 6)
-    x = decoder(src_feature, mask, tgt, tgt_mask)
-    assert list(x.shape) == [10, 10, 10]
+def test_sublayer_connection():
+    src, tgt_input, tgt_output, src_mask, tgt_mask = get_sample_input()
+    embed = Embedding(102, 50)
+    encoding = PositionalEncoding(50)
+    src = embed(src)
+    src = encoding(src)
+    ff = PositionwiseFeedforward(50, 100)
+    norm = LayerNorm(50)
+    sub = SublayerConnection(50)
+    src = sub(src, ff)
+    assert list(src.shape) == [10, 10, 50]
